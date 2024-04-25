7ef28e8da48ed1a4adf2c4629644217b58bcb497
jordan toujouse <jtoujo1@gmail.com>
2024-04-24 21:09:27 -0500

    created my module, installed babel, ran babel.

---
+++ src/index.js
@@ -0,0 +1,11 @@
+"use strict";
+
+import * as module1 from "./module1";
+
+import * as module2 from "./module2";
+
+console.log(module1); // { default: [Function: foo], bar
+
+console.log(module2); // { default: [Function: foo], bar
+
+const add = module1.add(1, 2)
---
+++ src/module2.js
@@ -0,0 +1,11 @@
+"use strict";
+
+const capitalLetters = (str) => {
+  return str.toUpperCase();
+};
+
+const smallLetters = (str) => {
+  return str.toLowerCase();
+};
+
+module.exports = { capitalLetters, smallLetters };
